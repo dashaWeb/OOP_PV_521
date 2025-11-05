28446d537ea0e2ae8796382c799a82c9e4205e83
konopelko_d <konopelko_d@itstep.academy>
2025-11-05 19:04:53 +0200

    template class

---
+++ 08_Template class/08_Template class.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "Array.h"
+using namespace std;
+
+
+//int sum(int a, int b)
+//{
+//    return a + b;
+//}
+//
+//double sum(double a, double b)
+//{
+//    return a + b;
+//}
+//
+//char sum(char a, char b)
+//{
+//    return a + b;
+//}
+
+//template <typename T>
+//T sum(T a, T b)
+//{
+//    return a + b;
+//}
+
+template<typename T, typename T2>
+struct Pair {
+	T first;
+	T2 last;
+	Pair() = default;
+	Pair(T first, T2 last)
+		:first{ first }, last{ last }
+	{}
+
+	template<typename T, typename T2>
+	friend ostream& operator <<(ostream& out, const Pair<T, T2> obj)
+	{
+		// (2,2.5)
+		out << "(" << obj.first << "," << obj.last << ")";
+		return out;
+	}
+};
+
+int main()
+{
+	/* cout << "Sum int :: " << sum(2, 3) << endl;
+	 cout << "Sum double :: " << sum(2.3, 3.8) << endl;
+	 cout << "Sum char :: " << sum('A', '1') << endl;
+	 */
+	Array<int> container(3);
+	container.print();
+
+	Array<double, 10> containerD(2.2);
+	containerD.print();
+
+	Array<char, 15> containerC('A');
+	containerC.print();
+
+	Pair<int, double> test(2, 5.2);
+	cout << test.first << " >> " << test.last << endl;
+
+	Array<Pair<int, double>, 20> containerPair(test);
+	containerPair.print();
+}
+
+
+
---
+++ 08_Template class/Array.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+
+template<typename T, size_t size = 5> // T - параметр типу, size = нетипований параметр (константа)
+class Array
+{
+public:
+	Array(const T& value = T());
+	void fill(const T& value = T());
+	const size_t& getSize() const;
+	void print() const;
+
+	T& operator[](size_t index);
+
+private:
+	
+	T arr[size];
+};
+
+template<typename T, size_t size>
+inline Array<T, size>::Array(const T& value)
+{
+	fill(value);
+}
+
+template<typename T, size_t size>
+inline void Array<T, size>::fill(const T& value)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = value;
+	}
+}
+
+template<typename T, size_t size>
+inline const size_t& Array<T, size>::getSize() const
+{
+	return size;
+}
+
+
+
+template<typename T, size_t size>
+inline void Array<T, size>::print() const
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+template<typename T, size_t size>
+inline T& Array<T, size>::operator[](size_t index)
+{
+	static T error;
+	if (index < size)
+		return arr[index];
+	cout << "\t\t Error Index :: operator[]" << endl;
+	return error;
+}
